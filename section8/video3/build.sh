c18fac50f6bcbb8153ecb185b4be93f49a5b1569
Daniel Arbuckle <djarb@highenergymagic.org>
2019-04-15 04:27:49 -0700

    fix build

--- section8/video3/build.sh
+++ section8/video3/build.sh
@@ -1,1 +1,1 @@
-pyinstaller --name=oware-client --add-data oware/client/data/:oware/client/data/ --add-data LICENSE:. --add-data README.md:. --noconsole main.py
+pyinstaller --name=oware-client --add-binary oware/client/data/:oware/client/data/ --add-data LICENSE:. --add-data README.md:. --noconsole main.py
